e2396e99c66a0a722c0abeb8e98d797a27d33825
WinstonH <bjdhjj@gmail.com>
2018-01-18 00:22:18 +0800

    Update codeanywhere.sh

--- codeanywhere.sh
+++ codeanywhere.sh
@@ -12,7 +12,7 @@ apt-get clean
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update
-apt-get install google-chrome-stable firefox-locale-zh-hans firefox midori lxterminal -y
+apt-get install google-chrome-stable firefox-locale-zh-hans firefox midori lxterminal xdotool -y
 apt-get clean
 
 wget https://raw.githubusercontent.com/getlantern/lantern-binaries/master/lantern-installer-64-bit.deb
